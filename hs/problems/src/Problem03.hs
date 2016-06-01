bda1628ba97685b8b94cfb415701ebeb45c4b8fe
Aaron Johnson <johnsoaab@gmail.com>
2016-05-31 23:31:15 -0700

    Add lazy primes method, solution to problem 3

--- hs/problems/src/Problem03.hs
+++ hs/problems/src/Problem03.hs
@@ -17,51 +17,81 @@ import qualified Data.Map as M
 
 data PrimeState = PrimeState {
     unsieved :: U.Vector Int,
+    pulled :: [Int],
     primes :: M.Map Int Int
 } deriving (Show)
 
--- 1) Make init -- 2 is presieved
-getInitVector :: Int -> U.Vector Int
-getInitVector upTo = U.generate (upTo `quot` 2 - 1) (\x -> x*2+3)
+together :: [a] -> [a] -> [a]
+together a b = foldr (:) b a
 
--- 2) Filter vector with a prime
+-- Get a vector of numbers without multiples of 2. Use a vector length and multiple.
+getInitVector :: Int -> Int -> U.Vector Int
+getInitVector upTo i = U.generate upTo (\x -> (i*upTo+x)*2+3)
+
+-- Filter a vector with a prime, starting at a specific multiple.
 primeSieve :: U.Vector Int -> Int -> Int -> (U.Vector Int, Int)
 primeSieve v prime startingAt =
     let lim = U.last v
-        cut = S.fromList [startingAt, startingAt + prime.. lim]
+        cut = S.fromList [startingAt, startingAt + prime ..lim]
     in (U.filter (\s -> s `S.notMember` cut) v, S.findMax cut)
 
--- 3) Given a prime map and an unsieved vector, assume the first element is a prime,
---    sieve the vector with it and pull it into the primes list
+-- Filter a vector with a prime-starter-multiple map, get back the sieved vector and the updated prime-starter-multiple map
+preSieve :: U.Vector Int -> M.Map Int Int -> (U.Vector Int, M.Map Int Int)
+preSieve toSieve sieveWith =
+    let getRange k a = [a, a+k ..(U.last toSieve)]
+        ranges = M.mapWithKey getRange sieveWith
+        newMap = M.map (\x -> last x) ranges
+        cut = foldl1 S.union (map S.fromList (M.elems ranges))
+    in (U.filter (\s -> s `S.notMember` cut) toSieve, newMap)
+
+-- Given a PrimeState, assume the first element is prime and recursively sieve the vector
 autoSieve :: PrimeState -> PrimeState
 autoSieve state
-    | U.length u == 0 = PrimeState { primes = ps, unsieved = u }
+    | U.length u == 0 = PrimeState { primes = ps, unsieved = u, pulled = puls }
     | otherwise =
         let p       = U.head u
             (psieved, biggest) = primeSieve u p p
-        in autoSieve PrimeState { unsieved = psieved, primes = M.insert p biggest ps }
+        in autoSieve PrimeState { unsieved = psieved, primes = M.insert p biggest ps, pulled = p:puls }
     where ps = primes state
           u = unsieved state
+          puls = pulled state
 
 getPrimesUnder :: Int -> [Int]
 getPrimesUnder num =
-    let init = getInitVector num
-        pkeys = M.keys $ primes (autoSieve PrimeState { primes = M.singleton 2 2, unsieved = init })
+    let init = getInitVector (num `quot` 2 - 1) 0
+        pkeys = M.keys $ primes (autoSieve PrimeState { primes = M.singleton 2 2, unsieved = init, pulled = [] })
     in pkeys
+
+-- something like::::
+getPrimes :: [Int]
+getPrimes =
+    let getMore i state =
+            let puls = pulled state
+                ps = primes state
+                (presieved, psUpdated) = preSieve (getInitVector 1000 i) ps
+                newState = autoSieve PrimeState { primes = psUpdated, unsieved = presieved, pulled = [] }
+            in together (reverse puls) (getMore (i + 1) newState)
+    in 2 : getMore 1 (autoSieve PrimeState { primes = M.empty, unsieved = getInitVector 1000 0, pulled = [] })
+    -- initial : more
 
 this `evenlyDivides` that = that `mod` this == 0
 
--- todo: to solve the problem, getting primes under 300 billion is not necessary; make the primes lazy
 getPrimeFactors :: Int -> [Int]
 getPrimeFactors number =
-    let primes = getPrimesUnder (number `quot` 2 + 1)
-        divide (v, p, agg, primes)
-            | p `evenlyDivides` v  = (v `quot` p, p, p : agg, primes)
-            | L.length primes == 1 && length agg == 0 = (1, v, [v], primes)
-            | otherwise            = (v, L.head primes, agg, tail primes)
-        stop = (\(v, _, _, _) -> v <= 1)
-        result = until stop divide (number, L.head primes, [], L.tail primes)
-    in (\(_, _, v, _) -> v) result
+    let divide n p
+            | n == p = ([n], 1)
+            | n == 1 = ([], 1)
+            | p `evenlyDivides` n =
+                let divided = n `quot` p
+                    (factors, num) = divide divided p
+                in (p : factors, num)
+            | otherwise = ([], n)
+        divideFold (prevFactors, curNum) nextPrime
+            | curNum == 1 = ([], 0)
+            | otherwise = (together newFactors prevFactors, nextNum)
+            where (newFactors, nextNum) = divide curNum nextPrime
+        primeScan = scanl divideFold ([], number) getPrimes
+    in fst $ last (takeWhile (\x -> snd x /= 0) primeScan)
 
-solve :: Integer
-solve = 23
+solve :: Int
+solve = head $ getPrimeFactors 600851475143
