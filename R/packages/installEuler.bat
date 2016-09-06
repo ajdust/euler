5f33ab89bbe5b3d41ad589fb4eb4f4b398478c24
Aaron Johnson <johnsoaab@gmail.com>
2016-09-05 20:41:32 -0700

    Add test for 3

---
+++ R/packages/installEuler.bat
@@ -0,0 +1,4 @@
+R CMD build euler
+R CMD INSTALL --library=C:/Users/aaron/Documents/R/win-library/3.2 euler_0.1.tar.gz
+
+
--- R/tests/tests.R
+++ R/tests/tests.R
@@ -1,10 +1,19 @@
 library(euler)
 library(RUnit)
+library(jsonlite)
+library(hash)
+
+ans <- fromJSON("../answers.json")
+ans <- lapply(ans, as.numeric)
 
 problem01_is_correct <- function() {
-	checkEquals(233168, problem01())
+	checkEquals(ans$`1`, problem01())
 }
 
 problem02_is_correct <- function() {
-	checkEquals(4613732, problem02())
+	checkEquals(ans$`2`, problem02())
+}
+
+problem03_is_correct <- function() {
+	checkEquals(ans$`3`, problem03())
 }
