02d025536522da9d955ad70a6aa6014b47c82a1c
Aaron Johnson <johnsoaab@gmail.com>
2017-03-20 00:08:40 -0700

    Add problems 1 to 10

---
+++ go/src/problems/problems_test.go
@@ -0,0 +1,116 @@
+package problems
+
+import (
+	"testing"
+	"io/ioutil"
+	"encoding/json"
+	"os"
+)
+
+
+var answers map[int]string
+
+func TestMain(m *testing.M) {
+	data, err := ioutil.ReadFile("../../../answers.json")
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(data, &answers)
+	if err != nil {
+		panic(err)
+	}
+
+	code := m.Run()
+	os.Exit(code)
+}
+
+func TestProblem01(t *testing.T) {
+	answer := Problem01()
+	if answer != answers[1] {
+		t.Error("Found", answer, "expected", answers[1])
+	} else {
+		t.Log("Found " + answer + " successfully")
+	}
+}
+
+func TestProblem02(t *testing.T) {
+	answer := Problem02()
+	if answer != answers[2] {
+		t.Error("Found", answer, "expected", answers[2])
+	} else {
+		t.Log("Found " + answer + " successfully")
+	}
+}
+
+func TestProblem03(t *testing.T) {
+	answer := Problem03()
+	if answer != answers[3] {
+		t.Error("Found", answer, "expected", answers[3])
+	} else {
+		t.Log("Found " + answer + " successfully")
+	}
+}
+
+func TestProblem04(t *testing.T) {
+	answer := Problem04()
+	if answer != answers[4] {
+		t.Error("Found", answer, "expected", answers[4])
+	} else {
+		t.Log("Found " + answer + " successfully")
+	}
+}
+
+func TestProblem05(t *testing.T) {
+	answer := Problem05()
+	if answer != answers[5] {
+		t.Error("Found", answer, "expected", answers[5])
+	} else {
+		t.Log("Found " + answer + " successfully")
+	}
+}
+
+func TestProblem06(t *testing.T) {
+	answer := Problem06()
+	if answer != answers[6] {
+		t.Error("Found", answer, "expected", answers[6])
+	} else {
+		t.Log("Found " + answer + " successfully")
+	}
+}
+
+func TestProblem07(t *testing.T) {
+	answer := Problem07()
+	if answer != answers[7] {
+		t.Error("Found", answer, "expected", answers[7])
+	} else {
+		t.Log("Found " + answer + " successfully")
+	}
+}
+
+func TestProblem08(t *testing.T) {
+	answer := Problem08()
+	if answer != answers[8] {
+		t.Error("Found", answer, "expected", answers[8])
+	} else {
+		t.Log("Found " + answer + " successfully")
+	}
+}
+
+func TestProblem09(t *testing.T) {
+	answer := Problem09()
+	if answer != answers[9] {
+		t.Error("Found", answer, "expected", answers[9])
+	} else {
+		t.Log("Found " + answer + " successfully")
+	}
+}
+
+func TestProblem10(t *testing.T) {
+	answer := Problem10()
+	if answer != answers[10] {
+		t.Error("Found", answer, "expected", answers[10])
+	} else {
+		t.Log("Found " + answer + " successfully")
+	}
+}
